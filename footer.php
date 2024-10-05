1cb8a25fc68ac845aac81820bd0449d663146e36
Hans Engren <hans@engren.se>
2024-10-05 10:13:54 +0200

    Updated footer with correct information to github.

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer class="bg-dark text-white text-center" style="padding: 5px 0;">
-    <p>HellsingeControl AB - hans@helcon.se</p>
+    <p><a href="https://github.com/engren/filamentmanager">https://github.com/engren/filamentmanager</a></p>
 </footer>
 
 <style>
